3f47854d9c4193858d8cef97e4ef1def9af42391
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2024-11-07 18:37:16 +0200

    Added option for sorting

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 	"math"
 	"net/http"
 	"os"
+	"os/exec"
+	"runtime"
+	"sort"
 	"strconv"
 	"strings"
 	"time"
@@ -49,10 +52,13 @@ type (
 		MasteryLevel int    `json:"mastery_level"`
 	}
 	RivenAuction struct {
-		Id          string            `json:"id"`
-		BuyoutPrice int               `json:"buyout_price"`
-		Owner       RivenAuctionOwner `json:"owner"`
-		Item        RivenAuctionItem  `json:"item"`
+		Id              string            `json:"id"`
+		BuyoutPrice     int               `json:"buyout_price"`
+		Owner           RivenAuctionOwner `json:"owner"`
+		Item            RivenAuctionItem  `json:"item"`
+		weapon          string            // Added later
+		endoGains       int               // Calculated later
+		endoPerPlatinum float64           // Calculated later
 	}
 	RivenAuctionPayload struct {
 		Auctions []RivenAuction `json:"auctions"`
@@ -61,6 +67,12 @@ type (
 		Payload RivenAuctionPayload `json:"payload"`
 	}
 )
+
+type PrintAuctionParameters struct {
+	auction    RivenAuction
+	itemName   string
+	silentMode bool
+}
 
 func logError(err error) {
 	errorLogger.Printf("%q", err)
@@ -108,17 +120,71 @@ func getAuctions(weapon string, minReRolls string) (*RivenAuctions, error) {
 	return rivenAuctions, nil
 }
 
+func printAuction(params PrintAuctionParameters) {
+	if !params.silentMode {
+		logger.Println()
+	}
+
+	logger.Printf("%s%s"+
+		"\n  -> Cost is %d platinum"+
+		"\n  -> Amount of re-rolls is %d"+
+		"\n  -> Mod rank is %d"+
+		"\n  -> Endo gains %d"+
+		"\n  -> Endo per platinum %.2f"+
+		"\n  -> %s is %s",
+		auctionRoute,
+		params.auction.Id,
+		params.auction.BuyoutPrice,
+		params.auction.Item.ReRolls,
+		params.auction.Item.ModRank,
+		params.auction.endoGains,
+		params.auction.endoPerPlatinum,
+		params.auction.Owner.IngameName,
+		params.auction.Owner.Status,
+	)
+
+	if params.auction.Owner.Status != "offline" {
+		logger.Printf("  -> /w %s Hi! Are you still selling the %s %s riven for %d:platinum:?",
+			params.auction.Owner.IngameName,
+			params.itemName,
+			params.auction.Item.Name,
+			params.auction.BuyoutPrice,
+		)
+	}
+
+	logger.Print("\n")
+}
+
+func clearScreen() {
+	var cmd *exec.Cmd
+
+	switch runtime.GOOS {
+	case "windows":
+		cmd = exec.Command("cmd", "/c", "cls")
+	default:
+		cmd = exec.Command("clear")
+	}
+
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
+
 func main() {
 	startTime := time.Now()
 
 	maxPrice := 50      // Maximum platinum price
 	minReRolls := "50"  // Minimum amount of re-rolls
 	silentMode := false // Only prints wanted auctions
+	sortOutput := false // Sort output based on endo per plat
 
 	positionalArg := 0
 	for _, arg := range os.Args[1:] {
-		if arg == "-s" {
+		switch arg {
+		case "-s":
 			silentMode = true
+			continue
+		case "-S":
+			sortOutput = true
 			continue
 		}
 
@@ -127,7 +193,6 @@ func main() {
 		switch positionalArg {
 		case 1:
 			_maxPrice, err := strconv.Atoi(arg)
-
 			if err != nil {
 				panic(err)
 			}
@@ -152,6 +217,8 @@ func main() {
 
 	auctionsSkipped := 0
 
+	foundAuctions := make([]PrintAuctionParameters, 0, 5)
+
 	for index, item := range rivenItems.Payload.Items {
 		rivenAuctions, err := getAuctions(item.UrlName, minReRolls)
 		if err != nil {
@@ -168,46 +235,32 @@ func main() {
 				continue
 			}
 
+			auction.weapon = item.Name
+
 			// math.Floor((100 × (MasteryLevel - 8) + 22.5 × 2^ModRank + 200 × ReRolls) - 7)
-			endoGains := int(math.Floor((100*(float64(auction.Item.MasteryLevel)-8) + 22.5*math.Pow(2, float64(auction.Item.ModRank)) + 200*float64(auction.Item.ReRolls)) - 7))
-			endoPerPlatinum := float64(endoGains) / float64(auction.BuyoutPrice)
+			auction.endoGains = int(math.Floor((100*(float64(auction.Item.MasteryLevel)-8) + 22.5*math.Pow(2, float64(auction.Item.ModRank)) + 200*float64(auction.Item.ReRolls)) - 7))
+			auction.endoPerPlatinum = float64(auction.endoGains) / float64(auction.BuyoutPrice)
 
-			if !silentMode {
-				logger.Println()
-			}
+			foundAuction := PrintAuctionParameters{auction: auction, itemName: item.Name, silentMode: silentMode}
+			foundAuctions = append(foundAuctions, foundAuction)
 
-			logger.Printf("%s%s"+
-				"\n  -> Cost is %d platinum"+
-				"\n  -> Amount of re-rolls is %d"+
-				"\n  -> Mod rank is %d"+
-				"\n  -> Endo gains %d"+
-				"\n  -> Endo per platinum %.2f"+
-				"\n  -> %s is %s",
-				auctionRoute,
-				auction.Id,
-				auction.BuyoutPrice,
-				auction.Item.ReRolls,
-				auction.Item.ModRank,
-				endoGains,
-				endoPerPlatinum,
-				auction.Owner.IngameName,
-				auction.Owner.Status,
-			)
-
-			if auction.Owner.Status != "offline" {
-				logger.Printf("  -> /w %s Hi! Are you still selling the %s %s riven for %d:platinum:?",
-					auction.Owner.IngameName,
-					item.Name,
-					auction.Item.Name,
-					auction.BuyoutPrice,
-				)
-			}
-
-			logger.Print("\n")
+			printAuction(foundAuction)
 		}
 
 		// Sleep to prevent being rate limited
-		time.Sleep(time.Second / 3)
+		time.Sleep(time.Millisecond * 350)
+	}
+
+	if sortOutput {
+		clearScreen()
+
+		sort.SliceStable(foundAuctions, func(i, j int) bool {
+			return foundAuctions[i].auction.endoPerPlatinum > foundAuctions[j].auction.endoPerPlatinum
+		})
+
+		for _, auction := range foundAuctions {
+			printAuction(auction)
+		}
 	}
 
 	runDuration := time.Since(startTime)
